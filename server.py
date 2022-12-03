bf0104ccb11c3257f7d9e576d39c6f226486b104
VAL099 <valeriy099@yahoo.com>
2022-12-03 20:28:19 +0200

    Initialising...

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import const, models, handlers, os
 
 server = FastAPI()
 
-db = mysql.connect(host = 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', port = '3306', user = 'jpj80vf0rrg10g3m', 
+db = msql.connect(host = 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', port = '3306', user = 'jpj80vf0rrg10g3m', 
                     password = 'p46enny4y03h8tcb', database = 'qpgekjgzhdj91u8j')
 cursor = db.cursor()
 
